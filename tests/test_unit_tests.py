771516f55bdd1f52710a8bacd91fe81d3ce3e451
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2025-03-17 17:24:08 +0000

    take out releative imports from testing

--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -1,12 +1,5 @@
-import sys
-from pathlib import Path
 import pytest
 from variantAnnotator import main, resources
-
-# Add the parent directory (project root) to sys.path using pathlib
-project_root = Path(__file__).resolve().parent.parent
-sys.path.append(str(project_root))
-
 
 def test_validate_genome_build_valid():
     assert main.validate_genome_build("GRCh37") == "GRCh37"
